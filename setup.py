998813f74c110f3b01dd268af2450b06f6a2e40c
smonm <e12021358@student.tuwien.ac.at>
2022-09-05 00:47:13 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+def readfile(filename):
+    with open(filename, "r+") as f:
+        return f.read()
+
+
+setup(
+    name="mousetravel",
+    version="1.0.0",
+    packages=[""],
+    url="https://github.com/Smonman/mousetravel",
+    license=readfile("LICENSE"),
+    author="Simon Josef Kreuzpointner",
+    author_email="simonkreuzpointner@gmail.com",
+    description="",
+    long_description=readfile("README.md"),
+    install_requires=["mouse~=0.7.1", "screeninfo~=0.8"],
+    scripts=["mousetravel.py"],
+    entry_points={
+        "console_scripts": [
+            "mousetravel = mousetravel:main"
+        ]
+    },
+)
